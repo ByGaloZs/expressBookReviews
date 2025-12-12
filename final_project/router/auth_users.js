8975fd01bc72d67ba3ad8d6f9e1f3482fcd0d2ed
Mario Padilla <mario-franco97@hotmail.com>
2025-12-11 22:30:44 -0600

    DELETE or deleting a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -94,6 +94,38 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({ message: "Review added/modified successfully", reviews: book.reviews});
 });
 
+// DELETE request: Delate the review of the logged-in user for a specific ISBN
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Extract ISBN fromm route parameters
+  const isbn = req.params.isbn;
+
+  // Get username from session authorization
+  const username = req.session.authorization ? req.session.authorization.username : null;
+
+  // Check if user is logged in
+  if(!username) {
+    return res.status(403).json({ message: "User not Logged in"});
+  }
+  
+  // Check if the book exists
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  // Check if the user has a review for the book
+  if (!book.reviews || !book.reviews[username]) {
+    return res.status(404).json({ message: "Review not found for this user" });
+  }
+
+  // Delete the user's review
+  delete book.reviews[username];
+
+  // Send success response
+  return res.status(200).json({ message: "Review deleted successfully", reviews: books[isbn].reviews});
+
+});
+
 // Export router and helper utilities used elsewhere in the app.
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
